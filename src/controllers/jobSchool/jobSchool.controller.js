6a6a08877fe832ed0cf7113a7607b5360fdfafd0
samuelAzurestech <abiokorigho.samuel@azurestech.com>
2021-03-21 16:30:04 -0700

    Update school Api

--- src/controllers/jobSchool/jobSchool.controller.js
+++ src/controllers/jobSchool/jobSchool.controller.js
@@ -9,47 +9,47 @@ export class JobController extends BaseController {
   }
 
   async createJob(req, res) {
- if(req.user.role !== 'school'){
-  return res.status(401).json({
-    message: 'You Are Not Approved To Perform This Action'
-  });
- }else{
-  const author = req.user._id
-    const data = req.body;
-
-    try {
-      const newJob = new Job(data);
-      const job = await newJob.save();
-      const finduser= await School.findById(author)
-      finduser.jobs.push(job)
-      await finduser.save();
-      job.owner.push(author)
-      await job.save()
-      return res.status(201).json({
-       job
-
+    if (req.user.role !== 'school') {
+      return res.status(401).json({
+        message: 'You Are Not Approved To Perform This Action'
       });
-    }
-    catch (e) {
-      super.error(e);
+    } else {
+      const author = req.user._id
+      const data = req.body;
+
+      try {
+        const newJob = new Job(data);
+        const job = await newJob.save();
+        const finduser = await School.findById(author)
+        finduser.jobs.push(job)
+        await finduser.save();
+        job.owner.push(author)
+        await job.save()
+        return res.status(201).json({
+          job
+
+        });
+      }
+      catch (e) {
+        super.error(e);
+      }
     }
   }
-}
 
   async readAllJob(req, res) {
-    if(req.user.approved !== true && req.user.status !== 'Approved'){
+    if (req.user.approved !== true && req.user.status !== 'Approved') {
       return res.status(400).send({ message: 'You Are Not Approved To Perform This Action' });
-    }else{
-    try {
+    } else {
+      try {
 
-      const job = await Job.find({});
+        const job = await Job.find({});
 
-      super.success(res, job || [], 'Successfully Retrieved all jobs.');
-    } catch (e) {
-      super.error(res, e);
+        super.success(res, job || [], 'Successfully Retrieved all jobs.');
+      } catch (e) {
+        super.error(res, e);
+      }
     }
   }
-}
 
   async deleteAllJob(req, res) {
     try {
@@ -66,58 +66,58 @@ export class JobController extends BaseController {
       if (!job) {
         return res.status(400).send({ error: 'Job does not exist' });
       }
-      if(job)
-      return res.status(200).send(job);
+      if (job)
+        return res.status(200).send(job);
     } catch (e) {
       super.error(res, e);
     }
   }
 
 
-async updateJob(req, res) {
-  if(req.user.approved !== true && req.user.status !== 'Approved'){
-    return res.status(400).send({ message: 'You Are Not Approved To Perform This Action' });
-  }else{
-  try {
-    const updates = Object.keys(req.body);
-    const allowedUpdates = [
-      ' neededTeacher',
-      'shortNoteAboutTeacherYouWant',
-    ];
-    const isValidUpdate = updates.every((update) => {
-      return allowedUpdates.includes(update);
-    });
+  async updateJob(req, res) {
+    if (req.user.approved !== true && req.user.status !== 'Approved') {
+      return res.status(400).send({ message: 'You Are Not Approved To Perform This Action' });
+    } else {
+      try {
+        const updates = Object.keys(req.body);
+        const allowedUpdates = [
+          'neededTeacher',
+          'shortNoteAboutTeacherYouWant',
+        ];
+        const isValidUpdate = updates.every((update) => {
+          return allowedUpdates.includes(update);
+        });
 
-    if (!isValidUpdate) {
-      throwError(400, 'Invalid Field.');
+        if (!isValidUpdate) {
+          throwError(400, 'Invalid Field.');
+        }
+
+        const jobUpdate = req.body;
+
+        updates.map((update) => {
+          req.user[update] = jobUpdate[update];
+        });
+
+        const updatedJob = await req.user.save();
+        super.success(res, updatedJob, 'Update Successful');
+      } catch (e) {
+        super.error(res, e);
+      }
     }
-
-    const jobUpdate = req.body;
-
-    updates.map((update) => {
-      req.user[update] = jobUpdate[update];
-    });
-
-    const updatedJob = await req.user.save();
-    super.success(res, updatedJob, 'Update Successful');
-  } catch (e) {
-    super.error(res, e);
   }
-}
-}
 
 
   async deleteOneJob(req, res) {
-    if(req.user.approved !== true && req.user.status !== 'Approved'){
+    if (req.user.approved !== true && req.user.status !== 'Approved') {
       return res.status(400).send({ message: 'You Are Not Approved To Perform This Action' });
-    }else{
-    try {
-      const job = await req.user.remove();
+    } else {
+      try {
+        await req.user.remove();
 
-      super.success(res, job, 'Delete Successful');
-    } catch (e) {
-      super.error(res, e);
+        super.success(res, 'Delete Successful');
+      } catch (e) {
+        super.error(res, e);
+      }
     }
   }
-}
 }
