b694286a8e08d546c92ad6b732d8ec905df6e939
samuelAzurestech <abiokorigho.samuel@azurestech.com>
2021-09-16 16:48:19 +0100

    Fix issue

--- src/controllers/teacher/index.js
+++ src/controllers/teacher/index.js
@@ -1,6 +1,7 @@
 export * from './health.controller';
 export * from './base.controller';
 export * from './teacher.controller';
-export * from '../school/school.controller'
+export * from '../school/school.controller';
 export * from './uploadVideoAndImage';
-export * from './connect.controller'
+export * from './connect.controller';
+///
